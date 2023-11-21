5648aba351b9cdf039b075c25d28aff321704cd1
Fathan <fathan.naufal@ui.ac.id>
2023-11-21 21:44:30 +0700

    csrf trusted origins

--- inventory_management/settings.py
+++ inventory_management/settings.py
@@ -150,3 +150,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CSRF_TRUSTED_ORIGINS = ['https://fathan-naufal-tugas.pbp.cs.ui.ac.id']
+
